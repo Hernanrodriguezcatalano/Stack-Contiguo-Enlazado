f6c6d2c590e06412e3dcb97aa5ccb733e3328e68
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-09 17:27:54 -0300

    Update Stack Contiguo.cpp

--- Stack Contiguo.cpp
+++ Stack Contiguo.cpp
@@ -28,16 +28,39 @@ int top(const stack& s) {
 
 }
 
-unsigned length(const stack& s) 
-{
+unsigned length(const stack& s) {
 	return s.i;
 }
 
-void vaciarStack(stack& s) 
-{
+void vaciarStack(stack& s) {
 	for (size_t i = 0; i < s.a.size(); i++)
 	{
 		s.a[i] = 0;
 	}
 	s.i = 0;
+}
+
+bool EsPalindromo(const stack& s){
+	//ir completando staux.a[j] 
+	assert(s.a[0] != NULL);
+	int min,max;
+	stack staux;
+	min = s.a[0];
+	max = s.a[0];
+	push(staux, s.a[0]);
+	for (size_t i = 1; i < s.a.size(); i++)
+	{
+		if (s.a[i]>=max)
+		{
+			max = s.a[i];
+		}
+		if (s.a[i]<=min)
+		{
+			min = s.a[i];
+		}
+		if (s.a[i]>=min && s.a[i] <= max)
+		{
+
+		}
+	}
 }
