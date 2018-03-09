98fd96922f0eba2183a209e2c1783de6b51b5f7b
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-09 16:45:38 -0300

    Update Stack Contiguo.cpp

--- Stack Contiguo.cpp
+++ Stack Contiguo.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "StackContiguo.h"
 #include<iostream>
 #include<cassert>
@@ -8,8 +9,8 @@ void push(stack& s , int x) {
 	assert(s.i < N);
 		s.a.at(s.i) = x;
 		++s.i;
-
 }
+
 int pop(stack& s) {
 	assert(s.i > 0);
 	{
@@ -18,26 +19,25 @@ int pop(stack& s) {
 	return s.a.at(s.i);
 }
 	
-
 int top(const stack& s) {
-	if (s.i > 0) {
+	assert(s.i > 0);
+	{
 		return s.a.at(s.i - 1);
 	}
-	else {
-		std::cout << "El Stack se encuentra vacio\n";
-		return 0;
-	}
+	
+
 }
 
-unsigned length(const stack& s) {
+unsigned length(const stack& s) 
+{
 	return s.i;
 }
 
-void vaciarStack(stack& s) {
-	for (size_t i = 0; i < s.a.size; i++)
+void vaciarStack(stack& s) 
+{
+	for (size_t i = 0; i < s.a.size(); i++)
 	{
 		s.a[i] = 0;
 	}
 	s.i = 0;
 }
-
