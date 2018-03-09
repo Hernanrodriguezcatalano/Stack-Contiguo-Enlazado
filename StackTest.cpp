689a6f5d924f95d64a4eacc7e6b4add206a7afec
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-09 16:43:57 -0300

    Update StackTest.cpp

--- StackTest.cpp
+++ StackTest.cpp
@@ -1,31 +1,44 @@
+#include "stdafx.h"
 #include <cassert>
 #include <iostream>
-#include <array>
 #include "StackContiguo.h"
+#include <Windows.h>
 using namespace std;
 
-array<int, 10> Istack;
-array<double, 5> Dstack;
-array<unsigned, 10> Ustack;
+
 
 stack Tstack;
-
+int a=0;
+int c;
 int main() {
-	cout << "Ingrese 10 numeros enteros";
-	for (size_t i = 0; i < Istack.size; i++)
-	{
-		assert(cin >> Istack[i]);
-	}
-	cout << "Ingrese 5 numeros reales";
-	for (size_t i = 0; i < Dstack.size; i++)
-	{
-		assert(cin >> Dstack[i]);
-	}
-	cout << "Ingrese 10 numeros positivos";
-	for (size_t i = 0; i < Ustack.size; i++)
-	{
-		assert(cin >> Ustack[i]);
-	}
-	cout << "No deben ingresarse mas de 5 ingresos numericos";
 	
-	push(Tstack, 10);
+	
+	cout << "Seleccione la funcion a testear:"<< endl<<"1.Push"<<endl<<"2.Pop"<<endl<<"3.Top"<<endl;
+	cin >> c;
+	assert(c > 0 && c < 4);
+	switch (c)
+	{
+	case 1:
+		while (a<N)
+		{
+		push(Tstack, rand() % 100);
+		cout << a << endl;
+		++a;
+		}
+		cout << "Intentando Pushear un elemento al Stack lleno...";
+		Sleep(3000);
+		push(Tstack, rand() % 100);
+	case 2:
+		vaciarStack(Tstack);
+		cout << "Intentando Popear un elemento del Stack vacio...";
+		Sleep(3000);
+		pop(Tstack);
+	case 3:
+		vaciarStack(Tstack);
+		cout << "Intentando Topear una Stack vacio...";
+		Sleep(3000);
+		top(Tstack);
+	}
+
+	
+}
